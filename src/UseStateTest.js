b2f07158c4d31e2e81f9275948f7f3f046e8ddd1
Safrin <mylifeofjava@gmail.com>
2024-03-10 08:40:37 +0800

    user Reducer

--- src/UseStateTest.js
+++ src/UseStateTest.js
@@ -1,18 +1,25 @@
 import { useReducer, useState } from "react"
 
-function reduxFn(oldCounter,step){
-    return oldCounter+step
+function reduxFn(oldCounter,action){
+    if(action==='increaseCounter'){
+        return {...oldCounter, counter:oldCounter.counter+oldCounter.steps}
+    }
+
+    if(action==='increaseSteps'){
+        return {...oldCounter, steps:oldCounter.steps+1}
+    }
 }
 
 
 function UseStateTest(){
-    const [counter,setCounter]=useReducer(reduxFn,2)
+    const [state,_setState]=useReducer(reduxFn,{counter:0,steps:1})
 
     
 
     return (
         <div className="App">
-        <h1 onClick={()=>setCounter(10)}>{counter}</h1>  
+        <h1 onClick={()=>_setState("increaseCounter")}>{state.counter}</h1>  
+        <h1 onClick={()=>_setState("increaseSteps")}>{state.steps}</h1>  
       </div>
     )
 }
